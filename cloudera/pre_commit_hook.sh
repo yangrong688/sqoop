0718f89146b3669e7c20fce7429e73828236b564
Szabolcs Vasas <vasas@cloudera.com>
2017-11-17 14:51:17 +0100

    CLOUDERA-BUILD: Change pre_commit_hook.sh to execute all test cases.
    Change-Id: I25bc3b33923c9cd88d9b5dcfe5a24c5b3fda085e
    (cherry picked from commit af1a6bc82d7acd4688651ddb83bacabcf97b807a)

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,5 +1,5 @@
-# CLOUDERA-BUILD
-export JAVA7_BUILD=true
-. /opt/toolchain/toolchain.sh
-
-ant clean test
+#!/bin/bash
+export MAVEN3_HOME=$MAVEN_3_5_0_HOME
+export JAVA7_HOME=$JAVA_1_7_HOME
+export ANT_HOME=$ANT_1_9_6_HOME
+./cloudera/test-stable.sh --build=build.xml --java=1.7 --target-java=1.7
