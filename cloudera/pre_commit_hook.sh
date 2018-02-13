81d1df30f839d1c76b565e33b4394293bd273ba8
Carl Myers <cmyers@cloudera.com>
2018-02-13 10:05:11 -0800

    RELENG-3084 - modify precommit hook to use mvn-gbn wrapper
    Change-Id: Id442b55ad2dd6caf56a1421ce5bad742efc2add3

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -13,11 +13,13 @@ trap cleanup_tp EXIT
 git fetch --depth=1 "$GIT_THIRD_PARTY_URL" refs/heads/master
 git archive --format=tar FETCH_HEAD:fpm/sqoop_3rdparty | (cd "$THIRDPARTY_LIBS" && tar -xv)
 
-export CDH_GBN=$(curl "http://builddb.infra.cloudera.com:8080/resolvealias?alias=cdh6.x")
-curl http://github.mtv.cloudera.com/raw/CDH/cdh/cdh6.x/gbn-m2-settings.xml > mvn_settings.xml
 export MAVEN3_HOME=$MAVEN_3_5_0_HOME
 export JAVA8_HOME=$JAVA_1_8_HOME
 export ANT_HOME=$ANT_1_9_6_HOME
 export PATH=$MAVEN3_HOME/bin:$PATH
-mvn -s mvn_settings.xml -f cloudera-pom.xml process-resources
+
+# activate mvn-gbn wrapper
+mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
+
+mvn -f cloudera-pom.xml process-resources
 ./cloudera/test-stable.sh --build=build.xml --java=1.8 --target-java=1.8
